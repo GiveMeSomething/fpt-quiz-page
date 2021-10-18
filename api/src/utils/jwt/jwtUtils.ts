d4a291d83426cafe324efe759f6ca8f628748606
GiveMeSomething <minhhthe150277@fpt.edu.vn>
2021-10-18 11:42:57 +0700

    update: update package.json scripts to lint and format staged files

--- api/src/utils/jwt/jwtUtils.ts
+++ api/src/utils/jwt/jwtUtils.ts
@@ -1,24 +1,23 @@
-import * as jwt from 'jsonwebtoken'
-import { UserArgs } from '../../@types/app.type'
+import * as jwt from 'jsonwebtoken';
+import { UserArgs } from '../../@types/app.type';
 
-import { User } from '../../models/user'
-import { RefreshTokenPayload, RefreshTokenResponse, JwtPayload } from '../../services/auth/auth.type'
-import { InternalServerException } from '../errorHandler/commonError'
-import { currentTimestampInSecond } from '../time'
+import { RefreshTokenPayload, RefreshTokenResponse, JwtPayload } from '../../services/auth/auth.type';
+import { InternalServerException } from '../errorHandler/commonError';
+import { currentTimestampInSecond } from '../time';
 
 // The time is in second(s) due to my configuration (not in 'ms' like jwt docs)
 export const getSecretKey = () => {
-    const secret = process.env.TOKEN_SECRET_KEY
+    const secret = process.env.TOKEN_SECRET_KEY;
 
     if (!secret) {
-        throw InternalServerException('Secret key not found')
+        throw InternalServerException('Secret key not found');
     }
 
-    return secret
-}
+    return secret;
+};
 
 function issueToken(payload: any, expiresIn: number | string): string {
-    return jwt.sign(payload, getSecretKey(), { expiresIn })
+    return jwt.sign(payload, getSecretKey(), { expiresIn });
 }
 
 // Currently use time as payload (will look into it later)
@@ -26,16 +25,16 @@ function issueRefreshToken(userId: string): RefreshTokenResponse {
     const payload: RefreshTokenPayload = {
         sub: userId,
         iat: currentTimestampInSecond(),
-    }
+    };
 
     // Refresh token can be used up to a week
-    const expiresIn = 7 * 24 * 60 * 60
+    const expiresIn = 7 * 24 * 60 * 60;
 
-    const token = issueToken(payload, expiresIn)
+    const token = issueToken(payload, expiresIn);
     return {
         accessToken: token,
         expires: expiresIn,
-    }
+    };
 }
 
 export function issueJwt({ userId, role }: UserArgs) {
@@ -43,17 +42,17 @@ export function issueJwt({ userId, role }: UserArgs) {
         sub: userId,
         iat: currentTimestampInSecond(),
         role,
-    }
+    };
 
     // Expires in 15 minutes
-    const expiresIn = 15 * 60
+    const expiresIn = 15 * 60;
 
-    const token = issueToken(payload, expiresIn)
+    const token = issueToken(payload, expiresIn);
 
     return {
         accessToken: token,
         refreshToken: issueRefreshToken(userId),
         expires: expiresIn,
         tokenType: 'Bearer',
-    }
+    };
 }
--- api/src/utils/time.ts
+++ api/src/utils/time.ts
@@ -6,15 +6,15 @@ const datetimeOptions = {
     hour: '2-digit',
     minute: '2-digit',
     second: '2-digit',
-} as const
+} as const;
 
 export const currentTimestampInString = (): string => {
-    const now = new Date()
-    const datetime = now.toLocaleString(undefined, datetimeOptions)
+    const now = new Date();
+    const datetime = now.toLocaleString(undefined, datetimeOptions);
 
-    return datetime
-}
+    return datetime;
+};
 
-export const currentTimestampInSecond = (): number => Math.floor(Date.now() / 1000)
+export const currentTimestampInSecond = (): number => Math.floor(Date.now() / 1000);
 
-export const isExpired = (timestamp: number): boolean => timestamp < currentTimestampInSecond()
+export const isExpired = (timestamp: number): boolean => timestamp < currentTimestampInSecond();
